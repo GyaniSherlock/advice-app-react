91b0af507543951061c78fb05211881d971148f7
GyaniSherlock <46041733+GyaniSherlock@users.noreply.github.com>
2022-04-19 17:57:26 +0530

    Removed API credentials

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import axios from 'axios'
 
 import './App.css';
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://cricket-live-data.p.rapidapi.com/match/2432999',
-//     headers: {
-//       'X-RapidAPI-Host': 'cricket-live-data.p.rapidapi.com',
-//       'X-RapidAPI-Key': 'e360e404d7mshbdab97809908aaep1d57dcjsn926674f837bf'
-//     }
-//   };
 class App extends React.Component{
     state = {
         advice: '',
